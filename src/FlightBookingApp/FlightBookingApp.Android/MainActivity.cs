b922e63d132e2a7b6fd4346d3e197d505a1457be
Javier Suárez Ruiz <javiersuarezruiz@hotmail.com>
2020-03-23 14:20:21 +0100

    Added SwipeView Experimental flag

--- src/FlightBookingApp/FlightBookingApp.Android/MainActivity.cs
+++ src/FlightBookingApp/FlightBookingApp.Android/MainActivity.cs
@@ -2,11 +2,12 @@
 using Android.Content.PM;
 using Android.Runtime;
 using Android.OS;
+using Xamarin.Forms;
 
 namespace FlightBookingApp.Droid
 {
     [Activity(Label = "FlightBookingApp", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
-    public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
+    public class MainActivity : Xamarin.Forms.Platform.Android.FormsAppCompatActivity
     {
         protected override void OnCreate(Bundle savedInstanceState)
         {
@@ -15,8 +16,10 @@ namespace FlightBookingApp.Droid
 
             base.OnCreate(savedInstanceState);
 
+            Forms.SetFlags("SwipeView_Experimental");
+
             Xamarin.Essentials.Platform.Init(this, savedInstanceState);
-            global::Xamarin.Forms.Forms.Init(this, savedInstanceState);
+            Forms.Init(this, savedInstanceState);
             LoadApplication(new App());
         }
 
